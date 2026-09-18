0c2d519f7a20ca5be7fd3e346829cdc24e666efb
teubragaa <teubraga2002@gmail.comgit config --global user.name>
2026-09-18 15:27:39 -0300

    Adiciona model Scan

---
+++ src/main/java/security/scanner/model/Scan.java
@@ -0,0 +1,31 @@
+package security.scanner.model;
+
+public class Scan {
+
+    private String url;
+    private String status;
+
+    public Scan() {
+    }
+
+    public Scan(String url, String status) {
+        this.url = url;
+        this.status = status;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
